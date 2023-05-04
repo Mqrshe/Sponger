ff977cd3126be66e15ccb5cffd6c0ba73cd55cfc
Mqrshie <spys23@hotmail.com>
2023-05-04 12:09:23 +0800

    yes.

--- src/main/java/me/mqrshe/sponger/Sponger.java
+++ src/main/java/me/mqrshe/sponger/Sponger.java
@@ -4,16 +4,16 @@ import me.mqrshe.sponger.handlers.RenderEventHandler;
 import me.mqrshe.sponger.managers.HUDManager;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.gui.screen.Screen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.lwjgl.glfw.GLFW;
+
 
 public class Sponger implements ModInitializer {
 
 	public static HUDManager hudManager;
 	public static Sponger IMPL = new Sponger();
 	public static final Logger LOGGER = LoggerFactory.getLogger("sponger");
+
 
 	@Override
 	public void onInitialize() {
---
+++ src/main/java/me/mqrshe/sponger/mixins/MinecraftServerMixin.java
@@ -0,0 +1,16 @@
+package me.mqrshe.sponger.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerChunkManager;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+    @Redirect(method = "prepareStartRegion", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;getTotalChunksLoadedCount()I"))
+    public int onPrepareStartReg_redirectChunksLoaded(ServerChunkManager scm) {
+        return 441;
+    }
+}
